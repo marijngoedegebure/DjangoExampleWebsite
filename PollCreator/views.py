0ff8ffdc0926a06bb7e522ba15e084e8c4f6d131
Marijn Goedegebure <marijngoedegebure@gmail.com>
2015-02-11 15:32:26 +0100

    Big update for the website, deleted bootstrap from the directories, installed it locally, added jquery using a CDN. Implemented the use of bootstrap to each view. Also created buttons and forms to add the different models from the frontend

--- PollCreator/views.py
+++ PollCreator/views.py
@@ -18,6 +18,59 @@ class IndexView(generic.ListView):
         return Questionnaire.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
+def newQuestion(request, questionnaire_id):
+    questionnaire = get_object_or_404(Questionnaire, id=questionnaire_id)
+    return render(request, 'PollCreator/newQuestion.html', {'questionnaire': questionnaire})
+
+
+def addQuestion(request, questionnaire_id):
+    try:
+        question_text = request.POST['question_text']
+    except (KeyError, question_text.DoesNotExist):
+        render(request, 'PollCreator/index.html')
+    else:
+        questionnaire = get_object_or_404(Questionnaire, id=questionnaire_id)
+        question = Question(question_text=question_text, questionnaire=questionnaire)
+        question.save()
+        questionnaire.save()
+        return HttpResponseRedirect(reverse('PollCreator:detailQuestionnaire', args={questionnaire_id}))
+
+
+def newChoice(request, questionnaire_id, question_id):
+    question = get_object_or_404(Question, id=question_id)
+    questionnaire = get_object_or_404(Questionnaire, id=questionnaire_id)
+    return render(request, 'PollCreator/newChoice.html', {'questionnaire': questionnaire, 'question': question})
+
+
+def addChoice(request, questionnaire_id, question_id):
+    try:
+        choice_text = request.POST['choice_text']
+    except (KeyError, choice_text.DoesNotExist):
+        render(request, 'PollCreator/index.html')
+    else:
+        question = get_object_or_404(Question, id=question_id)
+        questionnaire = get_object_or_404(Questionnaire, id=questionnaire_id)
+        choice = Choice(choice_text=choice_text, question=question)
+        choice.save()
+        question.save()
+        questionnaire.save()
+        return HttpResponseRedirect(reverse('PollCreator:detailQuestion', args={questionnaire_id, question_id}))
+
+def newQuestionnaire(request):
+    return render(request, 'PollCreator/newQuestionnaire.html')
+
+
+def addQuestionnaire(request):
+    try:
+        title = request.POST['title']
+    except (KeyError, title.DoesNotExist):
+        render(request, 'PollCreator/index.html')
+    else:
+        questionnaire = Questionnaire(title=title, pub_date=timezone.now())
+        questionnaire.save()
+        return HttpResponseRedirect(reverse('PollCreator:index'))
+
+
 def detailQuestion(request, questionnaire_id, question_id):
     question = get_object_or_404(Question, id=question_id)
     questionnaire = get_object_or_404(Questionnaire, id=questionnaire_id)
@@ -39,10 +92,12 @@ def resultsQuestion(request, questionnaire_id, question_id):
 
 def vote(request, questionnaire_id, question_id):
     question = get_object_or_404(Question, pk=question_id)
+    questionnaire = get_object_or_404(Questionnaire, id=questionnaire_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
-        return render(request, 'PollCreator/detail.html', {
+        return render(request, 'PollCreator/detailQuestion.html', {
+            'questionnaire': questionnaire,
             'question': question,
             'error_message': "You didn't select a choice.",
         })
