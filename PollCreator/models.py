1538ac31135a472a97be8c15f494e40a10f41ccf
Marijn Goedegebure <marijngoedegebure@gmail.com>
2015-02-11 16:54:27 +0100

    Got rid of a useless comment

--- PollCreator/models.py
+++ PollCreator/models.py
@@ -3,7 +3,6 @@ from django.utils import timezone
 from django.db import models
 
 
-# Create your models here.
 class Questionnaire(models.Model):
     title = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
